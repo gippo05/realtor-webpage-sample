aa6b612dd5244199ea468f697132d8285c29b3f1
gippo05 <gsarinas17@gmail.com>
2025-04-05 16:31:06 +0800

    added css

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showSideBar(){
+    const sidebar = document.getElementById('burger-menu')
+    sidebar.style.display = "flex"
+}
+
+function closeSideBar(){
+    const sidebar = document.getElementById('burger-menu')
+    sidebar.style.display = "none"
+}
