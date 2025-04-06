0bff3cdf392f963ad23ca1bbba5d5fcc0c075c44
gippo05 <gsarinas17@gmail.com>
2025-04-07 01:27:40 +0800

    Rendered final design for Desktop/Laptop View

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
+// footer year
 const date = new Date()
 let year = date.getFullYear();
 document.getElementById('year').innerHTML = year;
 
-
-
-
-
-
-
+// NAVBAR Mechanics
 function showSideBar(){
     const sidebar = document.getElementById('burger-menu')
     sidebar.style.display = "flex"
@@ -17,3 +13,55 @@ function closeSideBar(){
     const sidebar = document.getElementById('burger-menu')
     sidebar.style.display = "none"
 }
+
+// Animations
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    const profpic = document.getElementById('profpic');
+    const abouther = document.getElementById('abouther');
+    
+
+
+    if (scrolled > 11){
+        profpic.classList.add("slideRight");
+        abouther.classList.add("slideLeft");
+    }
+    else{
+        profpic.classList.remove("slideRight");
+        abouther.classList.remove("slideLeft");
+    }
+    // alert(scrolled);
+    
+});
+
+window.addEventListener('scroll', () => {
+
+    const scrolled = window.scrollY;
+    const div1 = document.getElementById('div1');
+    const div2 = document.getElementById('div2');
+    const div3 = document.getElementById('div3');
+    const div4 = document.getElementById('div4');
+    const div5 = document.getElementById('div5');
+    const div6 = document.getElementById('div6');
+
+    if (scrolled >= 1700){
+        div1.classList.add("slideRight2");
+        div2.classList.add("slideLeft2");
+    }
+    if(scrolled >= 2100){
+        div3.classList.add("slideRight2")
+        div4.classList.add("slideLeft2");
+    }
+    if(scrolled >= 2400){
+        div5.classList.add("slideRight2")
+        div6.classList.add("slideLeft2");
+    }
+    // else{
+    //     div1.classList.remove("slideRight2");
+    //     div2.classList.remove("slideLeft2");
+    //     div3.classList.remove("slideRight2");
+    //     div4.classList.remove("slideLeft2");
+    //     div5.classList.remove("slideRight2");
+    //     div6.classList.remove("slideLeft2");
+    // }
+});
